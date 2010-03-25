c9d07ef9ed46712bc4a86e0144bff0270aec14ad
elek <elek@localhost>
2010-03-25 19:46:29 +0100

    label figures init

---
+++ renderer/src/main/java/net/anzix/kogutowicz/style/LabelFigure.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.anzix.kogutowicz.style;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.anzix.kogutowicz.Zoom;
+import net.anzix.kogutowicz.element.Element;
+import net.anzix.kogutowicz.geometry.GeometryElement;
+import net.anzix.kogutowicz.geometry.Text;
+
+/**
+ *  Simple Label element
+ *
+ * @author elek
+ */
+public class LabelFigure extends Figure {
+
+    private String tag;
+
+    @Override
+    public List<GeometryElement> drawElements(Element element, Zoom zoom) {
+        List<GeometryElement> result = new ArrayList();
+        Text t = new Text();
+//        t.setText(element.tagValue(tag));
+        t.setText("UNKNOWN");
+        result.add(t);
+        return result;
+    }
+
+    @Override
+    public void init(String... parameters) {
+    }
+}
